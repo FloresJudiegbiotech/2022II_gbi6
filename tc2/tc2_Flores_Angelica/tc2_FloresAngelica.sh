6d215e8b7a671e2a89595df9d58aca1bece63170
FloresJudiegbiotech <117690831+FloresJudiegbiotech@users.noreply.github.com>
2022-11-09 18:48:11 -0500

    Actualización del deber ya que habían datos erróneos en el anterior
    Modificado por Angelica Flores

---
+++ tc2/tc2_Flores_Angelica/tc2_FloresAngelica.sh
@@ -0,0 +1,9 @@
+echo "7 primeras filas y las 5 ultimas filas"
+echo -e "\n"
+echo "7 primeras filas"
+echo -e " \n"
+head -n 7 EigenBetw.tsv
+echo -e " \n"
+echo "5  ultias filas"
+echo -e " \n"
+tail -n 5 EigenBetw.tsv
---
+++ tc2/tc2_Flores_Angelica/tc2_FloresAngelica2.sh
@@ -0,0 +1,8 @@
+echo " terminando en 5"
+
+echo -e "\n"
+
+grep AGAP[0-9]*5, EigenBetw.tsv | sort -t "," -k 3 -g > EigenBetw5.csv
+
+
+
